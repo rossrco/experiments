a8da93e2275a73c7fe02af82e9b6dc2399d6ce55
rossrco <rossrco@gmail.com>
2018-10-14 16:27:56 +0200

    separate the fit and transform methods Previously a single fit_transform method was available. Now the methods are separated into fit and transform. This is done in order to avoid feature leaking when transforming the entire dataset. Example: the entire set is sent to the fit_transform method, this means that the method has access to the test as well as the train subsets (which are not separated at that time). In addition, only the transformation function is now saved in the best_cols property. When the tranform method is called, the transformation functions saved in the best_cols property are called one by one on the corresponding features.

--- machine_learning_combinatorics/feature_engineer.py
+++ machine_learning_combinatorics/feature_engineer.py
@@ -1,6 +1,3 @@
-import pandas as pd
-import numpy as np
-from collections import deque
 from operator import add, mul, sub, truediv
 
 def update_n_best(best_cols, n_best, current_score, res_df):
@@ -23,10 +20,9 @@ class FeatureEngineer:
         self.model = model
         self.scorer = scorer
         self.n_best = min(n_best, 1000)
-        #self.best_cols = deque(maxlen = self.n_best)
         self.best_cols = {}
 
-    def fit_transform(self, df, y):
+    def fit(self, df, y):
         columns = df.columns
         best_score = 0
         worst_score = 0
@@ -36,29 +32,42 @@ class FeatureEngineer:
                 for c1 in columns:
                     for c2 in columns:
                         for o in self.operators:
+                            transformation_f = lambda x1, x2: o(e(x1), x2)
                             extremum = e(df[c1])
-                            res = o(extremum, df[c2])
+                            res = o(extremum, df[c2]).reshape(-1, 1)
                             colname = '%s_(%s)_%s_%s' % (e.__name__, c1, o.__name__, c2)
-                            res = res.to_frame(name = colname)
+                            transformation = {
+                            'transformation_function' : transformation_f,
+                            'x1' : c1,
+                            'x2' : c2,
+                            'column_name' : colname
+                            }
                             res_train, res_test, y_train, y_test = self.splitter(res, y, test_size = 0.3, random_state = 42)
                             self.model.fit(res_train, y_train)
                             y_pred = self.model.predict(res_test)
                             current_score = self.scorer(y_test, y_pred, average = 'weighted')
-                            self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, res)
+                            self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, transformation)
 
         for c1 in columns:
             for c2 in columns:
                 if c1 != c2:
                     for o in self.operators:
+                        transformation_f = lambda x1, x2: o(x1, x2)
                         colname = '%s_%s_%s' % (c1, o.__name__, c2)
-                        res = pd.DataFrame(o(df[c1], df[c2]), columns = [colname])
+                        transformation = {
+                        'transformation_function' : transformation_f,
+                        'x1' : c1,
+                        'x2' : c2,
+                        'column_name' : colname
+                        }
+                        res = o(df[c1], df[c2]).reshape(-1, 1)
                         res_train, res_test, y_train, y_test = self.splitter(res, y, test_size = 0.3, random_state = 42)
                         self.model.fit(res_train, y_train)
                         y_pred = self.model.predict(res_test)
                         #y_pred_proba = self.model.predict_proba(res_test)
                         current_score = self.scorer(y_test, y_pred, average = 'weighted')
-                        self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, res)
+                        self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, transformation)
 
-        for k, i in self.best_cols.items():
-            df = df.join(self.best_cols[k])
-        return df
+    def transform(self, df):
+        for k, trans in self.best_cols.items():
+            df[trans['column_name']] = trans['transformation_function'](df[trans['x1']], df[trans['x2']])
