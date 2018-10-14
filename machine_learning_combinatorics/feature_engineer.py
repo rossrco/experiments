4cd96f1f3cac65c97ce3cb78290d504a7704ea81
rossrco <rossrco@gmail.com>
2018-10-14 12:55:43 +0200

    reduce space complexity of the n best columns algorithm When new features are engineered, the features are kept in a dictionary. The dictionary is then sorted by key (score based on the slected metric). Previously all possible columns were kept in memory. Now only the n best columns are kept in memory and others are discarded if they perform worse than the worst score in the dictionary.

--- machine_learning_combinatorics/feature_engineer.py
+++ machine_learning_combinatorics/feature_engineer.py
@@ -1,6 +1,16 @@
 import pandas as pd
+import numpy as np
 from collections import deque
 from operator import add, mul, sub, truediv
+
+def update_n_best(best_cols, n_best, current_score, res_df):
+    worst_score = 0
+    best_cols[current_score] = res_df
+
+    if len(best_cols) > n_best:
+        worst_score = min(best_cols.keys())
+        best_cols.pop(worst_score)
+    return best_cols
 
 class FeatureEngineer:
     def __init__(self, splitter, model, scorer, functions = [add, mul, sub, truediv, max, min], n_best = 5):
@@ -12,12 +22,14 @@ class FeatureEngineer:
         self.splitter = splitter
         self.model = model
         self.scorer = scorer
-        self.n_best = n_best
+        self.n_best = min(n_best, 1000)
+        #self.best_cols = deque(maxlen = self.n_best)
         self.best_cols = {}
 
     def fit_transform(self, df, y):
         columns = df.columns
         best_score = 0
+        worst_score = 0
 
         if self.extrema:
             for e in self.extrema:
@@ -32,7 +44,7 @@ class FeatureEngineer:
                             self.model.fit(res_train, y_train)
                             y_pred = self.model.predict(res_test)
                             current_score = self.scorer(y_test, y_pred, average = 'weighted')
-                            self.best_cols[current_score] = res
+                            self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, res)
 
         for c1 in columns:
             for c2 in columns:
@@ -45,9 +57,8 @@ class FeatureEngineer:
                         y_pred = self.model.predict(res_test)
                         #y_pred_proba = self.model.predict_proba(res_test)
                         current_score = self.scorer(y_test, y_pred, average = 'weighted')
-                        self.best_cols[current_score] = res
+                        self.best_cols = update_n_best(self.best_cols, self.n_best, current_score, res)
 
-        for i, s in enumerate(sorted(self.best_cols, reverse = True)):
-            if i < self.n_best:
-                df = df.join(self.best_cols[s])
+        for k, i in self.best_cols.items():
+            df = df.join(self.best_cols[k])
         return df
