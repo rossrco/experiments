26cf3464e4d58ce9f57f2ae6ee804cf37bda83ec
rossrco <rossrco@gmail.com>
2018-11-25 14:27:31 +0100

    document the column-operator generator function

--- machine_learning_combinatorics/feature_engineer.py
+++ machine_learning_combinatorics/feature_engineer.py
@@ -2,9 +2,17 @@ from operator import add, mul, sub, truediv
 from numpy import isnan, isinf, isneginf
 from collections import defaultdict
 
-def update_n_best(best_columns, n_best, current_score, transformation):
+def update_n_best(best_columns, n_best, current_score, feature):
+    """
+    Takes: best columns dictionary, maximum number of columns to produce,
+    current feature's score, current feature.
+    Does: adds the current feature to the best columns dictionary. Removes the
+    lowest performing feature if the number of items in the dictionary exceeds
+    the maximum number of columns to produce.
+    Returns: best columns dictionary
+    """
     worst_score = 0
-    best_columns[current_score] = transformation
+    best_columns[current_score] = feature
 
     if len(best_columns) > n_best:
         worst_score = min(best_columns.keys())
@@ -12,6 +20,12 @@ def update_n_best(best_columns, n_best, current_score, transformation):
     return best_columns
 
 def apply_feature(df, feature, replace_inf):
+    """
+    Takes: dataframe, feature dictionary value to replace inf with
+    Does: uses the columns from the dataframe required by the feature. Produces
+    a new feature based on the transformation function.
+    Returns: numpy array representing the values of the new feature.
+    """
     res = feature['transformation_function'](df[feature['x1']], df[feature['x2']]).values.reshape(-1, 1)
     res[isinf(res)] = replace_inf
     res[isneginf(res)] = replace_inf
@@ -20,6 +34,15 @@ def apply_feature(df, feature, replace_inf):
 
 def get_current_score(x, y, model, scorer, splitter, predict_proba,
 scorer_kwargs = None, splitter_kwargs = None):
+    """
+    Takes: feature, target, supervised learning algorithm (e.g. logistic
+    regression), testing function (e.g. f1 score), splitting function (e.g.
+    train-test split), arguments for the last 3.
+    Does: splits the feature and the target into a training and validation sets.
+    Fits the model using the feature to the target. Predicts the target's values
+    for the validation set. Tests the predictions against the ground truth.
+    Returns: the score of the predictions based on the testing function.
+    """
     if splitter_kwargs:
         x_train, x_test, y_train, y_test = splitter(x, y, **splitter_kwargs)
     else:
@@ -38,6 +61,20 @@ scorer_kwargs = None, splitter_kwargs = None):
     return current_score
 
 def define_operator_generator(aggregates = None):
+    """
+    Takes: aggregate operators list.
+    Does: creates a generator of iterables to be used in a feature. An aggregate
+    feature is of the form in prefix notation: operator(aggregate(x1), x2).
+    Example: max(x1) / x2. To achieve that, the function iterates over all
+    aggregate operators, all columns against all columns and all operators. The
+    result is an aggregate operator, column1, column2 and an operator.
+    The other form of features is: operator(x1, x2), for example: x1 * x2.
+    To produce such features, the function needs to iterate over every column
+    against every other column (discarding repeating combinations) and over
+    every operator.
+    Returns: a generator that yeilds a dictionary of feature components
+    (aggregate operator, operator, column1, column2).
+    """
     if aggregates:
         def feature_item_generator(columns, operators, aggregates):
             for a in aggregates:
@@ -56,6 +93,11 @@ def define_operator_generator(aggregates = None):
     return feature_item_generator
 
 def define_feature_composer(aggregates = None):
+    """
+    Takes:
+    Does:
+    Returns:
+    """
     if aggregates:
         def feature_composer(feature_properties):
             column1 = feature_properties['c1']
@@ -84,6 +126,11 @@ def define_feature_composer(aggregates = None):
 def derive_feature_combinations(df, y, operators, best_columns, model, scorer,
 splitter, n_best, replace_inf, predict_proba, aggregates = None,
 scorer_kwargs = None, splitter_kwargs = None):
+    """
+    Takes:
+    Does:
+    Returns:
+    """
     generate_items = define_operator_generator(aggregates)
     compose_feature = define_feature_composer(aggregates)
     columns = df.columns
@@ -115,7 +162,11 @@ class FeatureEngineer:
         self.best_columns = {}
 
     def fit(self, df, y):
-
+        """
+        Takes:
+        Does:
+        Returns:
+        """
         if self.aggregates:
             self.best_columns = derive_feature_combinations(df, y, self.operators,
             self.best_columns, self.model, self.scorer, self.splitter, self.n_best,
@@ -128,5 +179,10 @@ class FeatureEngineer:
         self.splitter_kwargs)
 
     def transform(self, df):
+        """
+        Takes:
+        Does:
+        Returns:
+        """
         for k, trans in self.best_columns.items():
             df[trans['column_name']] = trans['transformation_function'](df[trans['x1']], df[trans['x2']])
